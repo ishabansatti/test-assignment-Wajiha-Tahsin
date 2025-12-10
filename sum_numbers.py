d5eed5513a8a84256e7de90033074a77cab2c0b9
Wajiha-Tahsin <192006.wajiha.tahsin@gmail.com>
2025-12-10 15:48:57 +0500

    Update sum_numbers.py

--- sum_numbers.py
+++ sum_numbers.py
@@ -3,6 +3,6 @@ def sum_numbers(*args):
     Returns the sum of all input numbers.
     If no arguments are passed, returns 0.
     """
-    # TODO: implement the logic
-    # total = sum(args)
+    #TODO: implement the logic
+    total = sum(args)
     return total
